052f9d8abc92faaf109ddeec2523607a67c35a9f
Danny Dong <danny.dong@ausregistry.com.au>
2014-03-19 13:15:39 +1100

    minor change.

--- core/src/test/scala/design/typeClass/TypeClassSpecTest.scala
+++ core/src/test/scala/design/typeClass/TypeClassSpecTest.scala
@@ -7,7 +7,7 @@ class TypeClassSpecTest extends FunSuite {
   test("type class example") {
 
     implicit val directShow = new Show[Int] {
-      override def show(x: Int) = 3
+      override def show(x: Int) = x
     }
 
     TypeClassSpec.process(3) === 3
